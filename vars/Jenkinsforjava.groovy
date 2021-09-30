ad29bf36827d03d4cf7213470b3651ac442a9932
AKBAR MOHAMMAD <43230334+AKBARMOHAMMAD@users.noreply.github.com>
2021-09-30 22:38:43 +0530

    Update Jenkinsforjava.groovy

--- vars/Jenkinsforjava.groovy
+++ vars/Jenkinsforjava.groovy
@@ -14,7 +14,7 @@ def call(String repoUrl){
         }
         stage('checkout code'){
             steps{
-                git branch: 'main',
+                git branch: 'master',
                 url : "${repoUrl}"
             }
         }
